33ef120db4a4e78d7ebd7350218aebba127b6efe
FaridlotunNIsak <38426321+FaridlotunNIsak@users.noreply.github.com>
2018-11-01 20:01:56 +0700

    Add files via upload

---
+++ TugasAkhir.py
@@ -0,0 +1,25 @@
+class HukumPajak:
+	def init(orang,jenis,pembayaran):
+		orang.jenis=jenis
+		orang.pembayaran=pembayaran
+jenis_pajak = array {["Kendaraan": "Roda 2", "Roda 3","Roda 4"],["Bangunan": "Rumah","Pertokoan","Perkantoran" ],["Tanah": "kurang dari 20 hektar","lebih dari 20 hektar" ]
+				 }				 
+tanya_pembayaran = input("Mau bayar pajak apa?")
+tanya_pembayaran = tanya_pembayaran.btsbawah()
+tanya_pembayaran = tanya_pembayaran.split(".")
+
+	def init(orang,pajak,Kendaraan);
+		orang.pajak=pajak
+		orang.bangunan=bangunan
+jenis_bangunan =[
+]
+
+kesimpulan =[]
+for pembayaran in tanya_pembayaran:
+	for pajak in jenis_pajak:
+		if pembayaran in jenis_pajak:
+			kesimpulan.append(pembayaran.jenis)
+		elif Baru:
+			print("anda sedang membayar:")
+		else:
+			print("Maaf anda sudah membayar")
